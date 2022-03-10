fd7451031bfeb7a65c8a614228f7a67ca6e350a9
Jonathan <jonwoods@umich.edu>
2022-03-10 14:47:35 -0500

    Updated AddIntegers in main.

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
+        int first = 10;
+        int second = 20;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
